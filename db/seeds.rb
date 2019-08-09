92f1e3aa21e1448c49f1a3af3347d319c8450a46
Garland Lai <garlandlai24@gmail.com>
2019-08-09 15:29:20 -0700

    add ':' to number.range to successfully create 50 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Product.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Food.ingredient,
-    cost: Faker::Number.within(range 1..20),
+    cost: Faker::Number.within(range: 1..20),
     country_of_origin: Faker::Address.country_code_long)
 end
 
