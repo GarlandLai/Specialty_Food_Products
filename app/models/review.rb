8de9b3fc4c408ad1d6d9ace96f7dc210f8fef82b
Garland Lai <garlandlai24@gmail.com>
2019-08-09 13:01:10 -0700

    add validation for integers

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :name, :rating, :content_body, presence: true
   validates_length_of :content_body, minimum: 50, maximum: 250
+  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5}
 end
