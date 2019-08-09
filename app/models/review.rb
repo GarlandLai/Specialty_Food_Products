d6d9a1e89084382cf41547203e82f8e6229b91d7
Garland Lai <garlandlai24@gmail.com>
2019-08-09 11:44:43 -0700

    specs of validations pass

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :name, :rating, :content_body, presence: true
+  validates_length_of :content_body, minimum: 50, maximum: 250
 end
