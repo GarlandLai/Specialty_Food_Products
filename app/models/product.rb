9518f308a80756dece1a26ecd0c91ef22404a3ee
Garland Lai <garlandlai24@gmail.com>
2019-08-10 14:31:44 -0700

    successful scopes and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,12 @@ class Product < ApplicationRecord
   validates :name, :cost, :country_of_origin, presence: true
   validates :cost, numericality: true
 
-  scope :product, -> {where(country_of_origin: "USA")}
+  scope :local, -> { (where(country_of_origin: "USA"))}
 
-  scope :three_most_recent, -> {order(created_at: :desc).limit(3)}
+  scope :most_recent, -> {order(created_at: :desc).limit(3)}
 
   scope :most_reviews, -> {(
-  select("products.id, products.name, products.cost, product.country_of_origin, count(reviews.id) as reviews_count")
+  select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count")
   .joins(:reviews)
   .group("products.id")
   .order("reviews_count DESC")
