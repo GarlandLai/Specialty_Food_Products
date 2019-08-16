ddd1776195b81b0fe8165001592a95af1ef064e4
Garland Lai <garlandlai24@gmail.com>
2019-08-16 10:34:46 -0700

    pass on first product integration test

--- spec/features/add_products_pages_spec.rb
+++ spec/features/add_products_pages_spec.rb
@@ -7,20 +7,26 @@ describe "the add a product" do
     click_link 'Register'
     fill_in "Email", :with => 'fake@gmail.com'
     fill_in "Password", :with => '123456'
+    fill_in "Password confirmation", :with => '123456'
     click_on 'Sign up'
-    save_and_open_page
     click_link 'Add a new product'
     fill_in 'Name', :with => 'Cheese'
     fill_in 'Cost', :with => 10
-    fill_in 'Country_of_origin', :with => 'USA'
+    fill_in 'Country of origin', :with => 'USA'
     click_on 'Create Product'
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Cheese'
   end
 
   # it "gives an error when no name is entered" do
-  #   visit new_album_path
-  #   click_on 'Create Album'
+  #   visit new_product_path
+  #   # save_and_open_page
+  #   fill_in "Email", :with => 'fake@gmail.com'
+  #   fill_in "Password", :with => '123456'
+  #   click_on 'Create Product'
   #   expect(page).to have_content "Name can't be blank"
+  #   expect(page).to have_content "Cost can't be blank"
+  #   expect(page).to have_content "Cost is not a number"
+  #   expect(page).to have_content "Country of origin can't be blank"
   # end
 end
