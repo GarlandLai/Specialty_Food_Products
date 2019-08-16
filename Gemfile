3798bd93fb236cfbef72f223cfaffa81a3f25ace
Garland Lai <garlandlai24@gmail.com>
2019-08-16 14:39:18 -0700

    clean up code and add simplecov

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ end
 group :test do
   gem 'shoulda-matchers'
   gem 'capybara'
+  gem 'simplecov', require: false
 end
 
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,14 +1,14 @@
-# require 'rails_helper'
-#
-# describe Product do
-#   it { should have_many(:reviews) }
-#   it { should validate_presence_of :name }
-#   it { should validate_presence_of :cost }
-#   it { should validate_presence_of :country_of_origin }
-#   it { should validate_numericality_of :cost }
-#
-#   it("it titleizes the name of the product") do
-#     product = Product.create({name: "aged gouda", cost: 10, country_of_origin: "USA"})
-#     expect(product.name()).to(eq("Aged Gouda"))
-#   end
-# end
+require 'rails_helper'
+
+describe Product do
+  it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
+  it { should validate_numericality_of :cost }
+
+  it("it titleizes the name of the product") do
+    product = Product.create({name: "aged gouda", cost: 10, country_of_origin: "USA"})
+    expect(product.name()).to(eq("Aged Gouda"))
+  end
+end
