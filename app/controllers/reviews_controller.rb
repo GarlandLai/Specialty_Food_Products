56f362f883958601a380ff3fe73054bb1b2d6d8d
Garland Lai <garlandlai24@gmail.com>
2019-08-16 09:22:58 -0700

    authenticate user is signed in to view specifics on products and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+before_action :authenticate_user!, :except => [:index]
   def new
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new
