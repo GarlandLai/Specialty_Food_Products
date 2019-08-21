933f8aaeec51771881ec0b6e4114b075172c66e8
Student <example@example.com>
2019-08-21 15:10:57 -0700

    pass 2nd product integration test

--- spec/features/add_products_pages_spec.rb
+++ spec/features/add_products_pages_spec.rb
@@ -18,15 +18,18 @@ describe "the add a product" do
     expect(page).to have_content 'Cheese'
   end
 
-  # it "gives an error when no name is entered" do
-  #   visit new_product_path
-  #   # save_and_open_page
-  #   fill_in "Email", :with => 'fake@gmail.com'
-  #   fill_in "Password", :with => '123456'
-  #   click_on 'Create Product'
-  #   expect(page).to have_content "Name can't be blank"
-  #   expect(page).to have_content "Cost can't be blank"
-  #   expect(page).to have_content "Cost is not a number"
-  #   expect(page).to have_content "Country of origin can't be blank"
-  # end
+  it "gives an error when no name is entered" do
+    visit products_path
+    click_link 'Register'
+    fill_in "Email", :with => 'fake@gmail.com'
+    fill_in "Password", :with => '123456'
+    fill_in "Password confirmation", :with => '123456'
+    click_on 'Sign up'
+    click_link 'Add a new product'
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Cost can't be blank"
+    expect(page).to have_content "Cost is not a number"
+    expect(page).to have_content "Country of origin can't be blank"
+  end
 end
